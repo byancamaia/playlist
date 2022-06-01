ea33be86f3a227b57ad5fbad26dea0e19468dd6e
Byanca Maia <byancacmaia@gmail.com>
2022-05-31 23:42:19 -0300

    fix: subindo projeto inicial

---
+++ src/test/java/br/anhembi/playlist/PlaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.anhembi.playlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
